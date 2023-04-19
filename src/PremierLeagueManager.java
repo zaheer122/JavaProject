1b3b4f6afd5dbf6c57d13b482f7407fee3cbf59a
zaheer122 <90451604+zaheer122@users.noreply.github.com>
2023-04-19 23:41:19 +0530

    Update PremierLeagueManager.java

--- src/PremierLeagueManager.java
+++ src/PremierLeagueManager.java
@@ -72,3 +72,55 @@ public class PremierLeagueManager implements LeagueManager {
         
     } 
    }
+ private void addTeam(){
+        if(league.size() == numberOfClubs) {
+            System.out.println("Can't add more clubs to league");
+            return;
+        }
+        
+        FootballClub club = new FootballClub();
+        System.out.println("Insert Club Name: ");
+        String line = scanner.nextLine();
+        club.setName(line);
+        
+        if(league.contains(club)){
+            System.out.println("This club is already in the league");
+            return;
+        }
+        System.out.println("Insert Club Location: ");
+        line = scanner.nextLine();
+        club.setLocation(line);
+        league.add(club);   
+    }
+    
+    private void deleteTeam() {
+        System.out.println("Insert club name: ");
+        String line = scanner.nextLine();
+         for(FootballClub club : league) {
+             if(club.getName().equals(line)){
+                 league.remove(club);
+                 System.out.println("Club "+ club.getName()+" removed");
+                 return;
+             }
+         }
+         System.out.println("No such club in league");
+    }
+    
+    private void displayStatistics() {
+        
+        System.out.println("Insert club name: ");
+        String line = scanner.nextLine();
+         for (FootballClub club : league) {
+             if(club.getName().equals(line)){
+                 System.out.println("Club " + club.getName()+ " matches won: " + club.getWinCount());
+                 System.out.println("Club " + club.getName()+ " matches lost: " + club.getDefeatCount());
+                 System.out.println("Club " + club.getName()+ " matches draw: " + club.getDrawCount());
+                 System.out.println("Club " + club.getName()+ " scored goals: " + club.getScoredGoalsCount());
+                 System.out.println("Club " + club.getName()+ " recieved goals: " + club.getReceivedGoalsCount());
+                 System.out.println("Club " + club.getName()+ " points: " + club.getPoints());
+                 System.out.println("Club " + club.getName()+ " matches played: " + club.getMatchesPlayed());
+                 return;
+             }
+         }
+         System.out.println("No such club in league");
+    } 
